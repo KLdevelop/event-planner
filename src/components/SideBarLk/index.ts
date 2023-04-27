b20365d37b57eb37b98b7a1f23f0d22c0c846004
houstonia~ <uwuowo733@gmail.com>
2023-04-27 12:22:10 +0300

    sidebar

---
+++ src/components/SideBarLk/index.ts
@@ -0,0 +1,1 @@
+export { SideBarLk } from './SideBarLk';
--- src/pages/PersonalPage/personalPage.tsx
+++ src/pages/PersonalPage/personalPage.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { SideBarLk } from 'src/components';
 import styles from './personalPage.module.scss';
 
 export const PersonalPage = () => {
-  return <div className={styles.personalPage}></div>;
+  return (
+    <div className={styles.personalPage}>
+      <SideBarLk
+        list={[
+          { name: 'Личная информация ', link: '/test' },
+          { name: 'Мои мероприятия ', link: '/test' },
+          { name: 'Моя команда', link: '/test' },
+          { name: 'Мой рейтинг', link: '/test' },
+        ]}
+      />
+    </div>
+  );
 };
