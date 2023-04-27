db060cc9dba6effd6319ffa528579ee76198c8ae
KLdevelop <drapdrop1@gmail.com>
2023-04-27 13:46:41 +0300

    link to navlink

--- src/components/SideBarLk/SideBarLk.tsx
+++ src/components/SideBarLk/SideBarLk.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './SideBarLk.module.scss';
 import { TabListElement } from 'src/types/interfaces';
-import { Link } from 'react-router-dom';
+import { NavLink as Link } from 'react-router-dom';
 
 //
 export const SideBarLk = (props: { list: TabListElement[] }) => {
@@ -9,7 +9,9 @@ export const SideBarLk = (props: { list: TabListElement[] }) => {
     <div className={styles.sideBar}>
       {props.list.map((i, ind) => (
         <div className={styles.sideBar_elem} key={ind}>
-          <Link to={i.link}>{i.name}</Link>
+          <Link to={`${i.link}`} relative="route">
+            {i.name}
+          </Link>
         </div>
       ))}
     </div>
