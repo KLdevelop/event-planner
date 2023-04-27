9c7e975422ba99e1b25b4b6d8dfd7c5654c6eb64
KLdevelop <drapdrop1@gmail.com>
2023-04-27 16:03:58 +0300

    background

--- src/pages/PersonalPage/personalPage.tsx
+++ src/pages/PersonalPage/personalPage.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { SideBarLk, Header, NameBlockLk } from 'src/components';
+import { SideBarLk, NameBlockLk } from 'src/components';
 import { Outlet } from 'react-router-dom';
 import styles from './personalPage.module.scss';
 
 export const PersonalPage = () => {
   return (
     <div className={styles.personalPage}>
-      <Header />
       <NameBlockLk />
       <div className={styles.mainBlock}>
         <div className={styles.tabBlock}>
