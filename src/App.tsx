14f6201185320b5a1d67579a0dafa5f63b14fca5
KLdevelop <drapdrop1@gmail.com>
2023-04-27 11:25:51 +0300

    personalpage

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from 'app.module.scss';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersonalPage } from './pages';
 
 export const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<div className={styles.app}>App</div>} />
+      <Route path="/lk" element={<PersonalPage />} />
     </Routes>
   </Router>
 );
---
+++ src/pages/PersonalPage/personalPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './personalPage.module.scss';
+
+export const PersonalPage = () => {
+  return <div className={styles.personalPage}></div>;
+};
