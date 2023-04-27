e3583cf1e5cf4892c428999bae52c02f6afd0a1d
houstonia~ <uwuowo733@gmail.com>
2023-04-27 16:30:06 +0300

    add header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import styles from './header.module.scss';
+import userLK from 'src/assets/logo/userLK.png';
+import headerLogo from 'src/assets/logo/headerLogo.svg';
 
 export const Header = () => {
-  return (<div className={styles.header}></div>);
-}
+  return (
+    <div className={styles.header}>
+      <div className={styles.header_left}>
+        <img src={headerLogo} />
+        <span>Программируя будущее</span>
+      </div>
+      <div className={styles.header_right}>
+        <div className={styles.header_right_img}>
+          <img src={userLK} />
+        </div>
+        <div className={styles.header_right_items}>
+          <span className={styles.header_right_span}>Главная</span>
+          <span className={styles.header_right_span}>Мероприятия</span>
+          <span className={styles.header_right_span}>Рейтинг</span>
+          <span className={styles.header_right_span}>Оценки</span>
+        </div>
+      </div>
+    </div>
+  );
+};
