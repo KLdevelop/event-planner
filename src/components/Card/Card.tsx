dcc08c3bf669c8da41d16728760a8279dcf5031c
houstonia~ <uwuowo733@gmail.com>
2023-04-27 15:26:13 +0300

    card final

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,13 +4,29 @@ interface CardProps {
   image: string;
   description: string;
   date: string;
+  team: string;
+  tags: string[];
 }
 export const Card = (props: CardProps) => {
   return (
     <div className={styles.card}>
-      <div className={styles.cardImage} style={{ backgroundImage: `url(${props.image})` }}></div>
-      <div>{props.description}</div>
-      <div>{props.date}</div>
+      <img src={props.image} />
+      <div className={styles.cardDesc}>{props.description}</div>
+      <div className={styles.cardDate}>
+        <span>Дата:</span>
+        {props.date}
+      </div>
+      <div className={styles.cardTeam}>
+        <span>Команда: </span>
+        {props.team}
+      </div>
+      <div className={styles.cardTag}>
+        {props.tags.map((i, ind) => (
+          <div className={styles.cardTagElem} key={ind}>
+            {i}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
