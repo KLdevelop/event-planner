37ab53614347ac3f88c6057ba8854786b9e0ad34
KLdevelop <drapdrop1@gmail.com>
2023-04-27 17:09:42 +0300

    header footer to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import styles from 'app.module.scss';
+import 'app.module.scss';
 import { Route, Routes } from 'react-router';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Outlet } from 'react-router-dom';
 import { PersonalPage, OverviewPage } from './pages';
+import { Header } from './components';
+import { Footer } from './components/Footer';
 
 export const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<div className={styles.app}>App</div>} />
-      <Route path="/overview" element={<OverviewPage />} />
-      <Route path="/lk" element={<PersonalPage />}>
-        <Route path="info" element={<div>Info</div>} />
-        <Route path="events" element={<div>Events</div>} />
-        <Route path="team" element={<div>Team</div>} />
-        <Route path="rating" element={<div>Rating</div>} />
-        <Route path="registr" element={<div>Registr</div>} />
-        <Route path="delete" element={<div>Delete</div>} />
-        <Route path="analysis" element={<div>Analysis</div>} />
-        <Route path="sendapp" element={<div>Send app</div>} />
-        <Route path="rating" element={<div>Rating</div>} />
+      <Route
+        path="/"
+        element={
+          <>
+            <Header />
+            <Outlet />
+            <Footer />
+          </>
+        }
+      >
+        <Route path="overview" element={<OverviewPage />} />
+        <Route path="lk" element={<PersonalPage />}>
+          <Route path="info" element={<div>Info</div>} />
+          <Route path="events" element={<div>Events</div>} />
+          <Route path="team" element={<div>Team</div>} />
+          <Route path="rating" element={<div>Rating</div>} />
+          <Route path="registr" element={<div>Registr</div>} />
+          <Route path="delete" element={<div>Delete</div>} />
+          <Route path="analysis" element={<div>Analysis</div>} />
+          <Route path="sendapp" element={<div>Send app</div>} />
+          <Route path="rating" element={<div>Rating</div>} />
+        </Route>
       </Route>
     </Routes>
   </Router>
