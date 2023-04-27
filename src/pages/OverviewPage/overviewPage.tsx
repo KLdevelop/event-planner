c2c34e900e0be12bbf265fdd263122e9b35c58d5
houstonia~ <uwuowo733@gmail.com>
2023-04-27 15:51:21 +0300

    cards and overview fix

--- src/pages/OverviewPage/overviewPage.tsx
+++ src/pages/OverviewPage/overviewPage.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import { Card, DatePoint } from 'src/components';
+import cardImage from 'src/assets/img/cardImage.png';
+import styles from './overviewPage.module.scss';
 
 export const OverviewPage = () => {
   return (
-    <>
+    <div className={styles.overviewPage}>
       <DatePoint />
-      <Card image="" description="" date="" />
-    </>
+      <div className={styles.overviewPage_card}>
+        <Card
+          image={cardImage}
+          description="Хакатон в рамках Всероссийских соревнований по спортивному программированию"
+          date="27.04.2023 "
+          team="от 3 до 5 участников"
+          tags={['AI/ML', 'Backend', 'Frontend', 'С++', 'Data Science']}
+        />
+        <Card
+          image={cardImage}
+          description="Хакатон в рамках Всероссийских соревнований по спортивному программированию"
+          date="27.04.2023 "
+          team="от 3 до 5 участников"
+          tags={['AI/ML', 'Backend', 'Frontend', 'С++', 'Data Science']}
+        />
+        <Card
+          image={cardImage}
+          description="Хакатон в рамках Всероссийских соревнований по спортивному программированию"
+          date="27.04.2023 "
+          team="от 3 до 5 участников"
+          tags={['AI/ML', 'Backend', 'Frontend', 'С++', 'Data Science']}
+        />
+        <Card
+          image={cardImage}
+          description="Хакатон в рамках Всероссийских соревнований по спортивному программированию"
+          date="27.04.2023 "
+          team="от 3 до 5 участников"
+          tags={['AI/ML', 'Backend', 'Frontend', 'С++', 'Data Science']}
+        />
+      </div>
+    </div>
   );
 };
